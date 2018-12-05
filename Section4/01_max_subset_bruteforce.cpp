5ca27fc43889b3b6548b548af210711f267abf98
Gulsah Kose <gulsah.1004@gmail.com>
2018-12-05 18:32:46 +0300

    Change array values and max_sum initializion

--- Section4/01_max_subset_bruteforce.cpp
+++ Section4/01_max_subset_bruteforce.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <limits.h>
 
 using namespace std;
 
 int main()
 {
-    int arr[] = {-2, 11, -4, 13, -5, 2};
+    //Page:68,  Section:4.1, Brute force solution
+
+    int arr[] = {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
     int length = (sizeof(arr) / sizeof(*arr));
     
-    int maxSum = 0;
+    int maxSum = INT_MIN;
     int begin, end;
 
     for(int i=0; i<length; i++)
